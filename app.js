1fde25a10e8a8ef1026b026796466f91821d1dae
lankada96 <lankadadivyanshi@gmail.com>
2022-12-03 12:08:54 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors=require('cors');
+app.use(cors());
+const dotenv = require("dotenv");
+dotenv.config();
+
+const UserRegisterRoute = require('./routes/registerUserRoute');
+const UserLoginRoute = require('./routes/userLoginRoute');
+
+
+
+
+
+
+
+app.use(bodyParser());
+app.use("/register", UserRegisterRoute);
+app.use("/login", UserLoginRoute);
+
+
+module.exports = app;
